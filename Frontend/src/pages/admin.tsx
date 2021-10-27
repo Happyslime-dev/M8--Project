06df68014d361e59e2da64f8e423673f3ae55840
Jaoearn <sutheera_preenan@cmu.ac.th>
2021-10-27 23:56:36 +0700

    Added Admin Route Protection

--- Frontend/src/pages/admin.tsx
+++ Frontend/src/pages/admin.tsx
@@ -1,5 +1,31 @@
+import { useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Loader from 'react-loader-spinner'
+
 import Admin from '../components/Admin'
+import { AuthContext } from '../context/AuthContextProvider'
+import { isAdmin } from '../helpers/authHelpers'
 
 export default function AdminPage() {
-  return <Admin />
+  const { loggedInUser } = useContext(AuthContext)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!loggedInUser) {
+      router.push('/')
+    } else {
+      if (!isAdmin(loggedInUser)) {
+        
+        router.push('/dashboard')
+        alert('No Authorization.')
+      }
+    }
+  }, [loggedInUser])
+
+  return !isAdmin(loggedInUser) ? (
+    <Loader type='Oval' color='teal' height={30} width={30} timeout={30000} />
+  ) : (
+    <Admin />
+  )
 }
