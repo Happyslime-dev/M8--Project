b74768286af87a5bd410ce11e84663da70cbb767
Jaoearn <sutheera_preenan@cmu.ac.th>
2021-10-28 00:19:27 +0700

    Added delete user

--- Frontend/src/components/AdminRow.tsx
+++ Frontend/src/components/AdminRow.tsx
@@ -6,7 +6,8 @@ import Loader from 'react-loader-spinner'
 
 import { User, Role } from '../types'
 import { isSuperAdmin } from '../helpers/authHelpers'
-import { UPDATE_ROLES } from '../apollo/mutations'
+import { UPDATE_ROLES, DELETE_USER } from '../apollo/mutations'
+import { QUERY_USERS } from '../apollo/queries'
 
 interface Props {
   user: User
@@ -50,6 +51,7 @@ const AdminRow: React.FC<Props> = ({ user, admin }) => {
         v ? newRoles.push(k as Role) : null
       ) // {ITEMEDITOR: true, ADMIN: false} --> [[ITEMEDITOR, true], [ADMIN, false]]
 
+      // Check if the user.roles array has not been changed --> do not call to backend
       if (user.roles.length === newRoles.length) {
         const checkRoles = user.roles.map((role) => newRoles.includes(role))
 
@@ -66,6 +68,31 @@ const AdminRow: React.FC<Props> = ({ user, admin }) => {
       }
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  const [deleteUser, deleteUserRes] = useMutation<
+    { deleteUser: { message: string } },
+    { userId: string }
+  >(DELETE_USER)
+
+  useEffect(() => {
+    if (deleteUserRes.error)
+      alert(deleteUserRes.error.graphQLErrors[0]?.message)
+  }, [deleteUserRes.error])
+
+  const handleDeleteUser = async (userId: string) => {
+    try {
+      const res = await deleteUser({
+        variables: { userId },
+        refetchQueries: [{ query: QUERY_USERS }],
+      })
+
+      if (res.data?.deleteUser.message) {
+        alert(res.data?.deleteUser.message)
+      }
+    } catch (error) {
+      alert((error as Error).message)
     }
   }
 
@@ -218,12 +245,29 @@ const AdminRow: React.FC<Props> = ({ user, admin }) => {
           )}
 
           <td>
-            <DeleteBtn
-              style={{ cursor: isEditing ? 'not-allowed' : undefined }}
-              disabled={isEditing}
-            >
-              <FontAwesomeIcon icon={['fas', 'trash-alt']} size='lg' />
-            </DeleteBtn>
+            {isSuperAdmin(user) ? null : (
+              <DeleteBtn
+                style={{ cursor: isEditing ? 'not-allowed' : undefined }}
+                disabled={isEditing}
+                onClick={() => {
+                  if (!confirm('Are you sure to delete this user?')) return
+
+                  handleDeleteUser(user.id)
+                }}
+              >
+                {deleteUserRes.loading ? (
+                  <Loader
+                    type='Oval'
+                    color='teal'
+                    width={30}
+                    height={30}
+                    timeout={30000}
+                  />
+                ) : (
+                  <FontAwesomeIcon icon={['fas', 'trash-alt']} size='lg' />
+                )}
+              </DeleteBtn>
+            )}
           </td>
         </>
       )}
