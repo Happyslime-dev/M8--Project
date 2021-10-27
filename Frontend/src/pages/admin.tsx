4a0d7438f25a124ceba5c79ba6976ecea86018b1
Jaoearn <sutheera_preenan@cmu.ac.th>
2021-10-28 00:15:53 +0700

    Added update roles

--- Frontend/src/pages/admin.tsx
+++ Frontend/src/pages/admin.tsx
@@ -12,11 +12,12 @@ export default function AdminPage() {
   const router = useRouter()
 
   useEffect(() => {
+    // If user in not authenticated, push to home page
     if (!loggedInUser) {
       router.push('/')
     } else {
       if (!isAdmin(loggedInUser)) {
-        
+        // Push user to their dashboard
         router.push('/dashboard')
         alert('No Authorization.')
       }
@@ -26,6 +27,6 @@ export default function AdminPage() {
   return !isAdmin(loggedInUser) ? (
     <Loader type='Oval' color='teal' height={30} width={30} timeout={30000} />
   ) : (
-    <Admin />
+    <Admin admin={loggedInUser} />
   )
 }
